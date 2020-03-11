7538e0cf531ca0d05e43a078590094b20b8634d1
ecaoili24 <erika.caoili@gmail.com>
2020-03-11 22:44:57 +0000

    Fixed the edge case and changed the order of the nodes

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,21 +4,24 @@
  * binary_tree_insert_left - inserts a node as a left-child of another node
  * @parent: pointer to parent
  * @value: value of node
+ *
  * Return: pointer to the created node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	new = malloc(sizeof(*new));
+	if (parent == NULL)
+		return (NULL);
 
-	new->parent = parent;
-	new->n = value;
-	new->left = parent->left;
-	if (new->left)
+	new = binary_tree_node(parent, value);
+
+	if (parent->left)
+	{
+		new->left = parent->left;
 		new->left->parent = new;
-	new->right = NULL;
-	parent->left = new;
+	}
 
+	parent->left = new;
 	return (new);
 }
