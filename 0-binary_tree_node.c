eeba27f3a2034ef9b6eccf1c210d8bac11719b0c
ecaoili24 <erika.caoili@gmail.com>
2020-03-11 22:31:57 +0000

    fixed betty issue

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,9 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_t - creates a binary tree node
+ * binary_tree_node - creates a binary tree node
  * @parent: pointer to parent
  * @value: value of node
+ *
  * Return: pointer to new node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
