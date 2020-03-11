eae4439500bd104648165cf72146d680e4b5a3e9
ecaoili24 <erika.caoili@gmail.com>
2020-03-11 22:13:03 +0000

    used calloc instead of malloc

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,13 +8,12 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = (binary_tree_t*) malloc(sizeof(binary_tree_t));
+	binary_tree_t *new = (binary_tree_t*) calloc(1, sizeof(binary_tree_t));
 
 	if (new == NULL)
 		return (NULL);
 
 	new->parent = parent;
 	new->n = value;
-	new->left = NULL;
 	return (new);
 }
